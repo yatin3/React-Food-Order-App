352b79ca651f77030ff9868bdc050dd7bbef8197
yatin goyal <yatingoyal31@gmail.com>
2023-12-26 15:51:19 +0530

    added and removed button handler

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import { useContext } from "react";
+import { useContext,useEffect,useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import "./HeaderCartButton.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted,setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber,item)=>{
     return curNumber + item.amount;
   },0);
 
+  const {items} = cartCtx;
+
+  let btnClasses = 'button ';
+  if (btnIsHighlighted){
+    btnClasses += 'bump';
+  }
+
+  useEffect(()=>{
+    if(items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(()=>{
+      setBtnIsHighlighted(false);
+    },300);
+
+    return ()=>{
+      clearTimeout(timer);
+    };
+  },[items]);
+
   return (
-    <button className="button" onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className="icon">
         <CartIcon />
       </span>
